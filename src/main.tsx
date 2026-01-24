f0988a47ccc0a361b1968c34c6c7f861388f22d2
KatherineHYC <KatherineHYC@users.noreply.github.com>
2026-01-24 23:26:06 +0800

    refactor: move Authgear init to main.tsx and remove authgear module

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import authgear from "@authgear/web";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function init() {
+  try {
+    await authgear.configure({
+      endpoint: import.meta.env.VITE_AUTHGEAR_ENDPOINT,
+      clientID: import.meta.env.VITE_AUTHGEAR_CLIENT_ID,
+      sessionType: "refresh_token",
+    });
+  } finally {
+    createRoot(document.getElementById("root")!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    );
+  }
+}
+
+init().catch((e) => {
+  console.error("Authgear init error:", e);
+});
